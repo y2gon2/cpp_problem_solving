bd03873778ba6f1d69000f39f0b0c247aff8fbe9
y2gon2 <y2gon2@gmail.com>
2024-12-08 17:21:55 +0900

    solved code at 24.12.08

--- beajoon/solved/sorting/241208_3_2075.cpp
+++ beajoon/solved/sorting/241208_3_2075.cpp
@@ -10,17 +10,15 @@
 
 using namespace std;
 
-
 int main() {
 	cin.tie(NULL);
 	cin.sync_with_stdio(false);
 
-	priority_queue<int, vector<int>, greater<int>> pq;
+	priority_queue<int, vector<int>, greater<int>> pq; // 내림차순 최소 힙 (힙 구조이므로 최상단 (외곽) 값이 가장 작은 값이 된다.)
 
 	int n;
 	cin >> n;
 	cin.ignore();
-
 
 	string input;
 	int num;
@@ -34,12 +32,14 @@ int main() {
 			ss >> num;
 			pq.push(num);
 
+			// 힙의 크기가 n 보다 커지면 그 위로 노출된 값 (크기로 내림차순하여 n 번째 값보다 작은 값) 은 탈락 시킴
 			if ((int)pq.size() > n) {
 				pq.pop();
 			}
 		}
 	}
 
+	// 최종 정리된 최소 힙 구조에서 최상단 (n 번째 크기 값) 을 출력
 	cout << pq.top() << endl;
 	return 0;
 }
